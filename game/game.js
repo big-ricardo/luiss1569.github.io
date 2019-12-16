d987aa98a56e87786691042904fec919fef75114
luiss1569 <rcsrcs1569@gmail.com>
2019-12-16 11:02:34 -0300

    add game da snake

--- game/game.js
+++ game/game.js
@@ -2,9 +2,10 @@
 let game
 const gameCanvas = document.getElementById('game-canvas')
 const context = gameCanvas.getContext('2d')
-game = { status: false}
+game = { status: false }
 document.addEventListener('keydown', handleKeydown)
 function main() {
+    document.getElementById('play').classList.add('some')
     init()
 }
 
@@ -15,8 +16,8 @@ function playerRender() {
     context.fillStyle = '#050505'
     context.globalAlpha = 1
     context.fillRect(game.player.x, game.player.y, game.player.width, game.player.heigth)
-    
-    for(let i = 0; i < game.player.tail.length; i++){
+
+    for (let i = 0; i < game.player.tail.length; i++) {
         context.fillStyle = '#050505'
         context.globalAlpha = 0.3
         context.fillRect(game.player.tail[i].x, game.player.tail[i].y, game.player.width, game.player.heigth)
@@ -47,7 +48,7 @@ function init() {
         fruit: null,
         status: true
     }
-    if(screen.width < 768){
+    if (screen.width < 768) {
         document.querySelector('#control').classList.remove('some')
     }
     playerRender(game.move)
@@ -56,32 +57,36 @@ function init() {
 function handleKeydown(e) {
     const keyPress = e.key
 
-    if (keyPress === 'ArrowDown'  && game.moved !== 'top') {
+    if (keyPress === 'ArrowDown' && game.moved !== 'top') {
         game.move = 'botton'
-        if(game.player.points > 0){
+        if (game.player.points > 0) {
             game.moved = game.move
         }
-    }
-    if (keyPress === 'ArrowLeft'  && game.moved !== 'rigth') {
-        game.move = 'left'
-        if(game.player.points > 0){
-            game.moved = game.move
+    } else {
+        if (keyPress === 'ArrowLeft' && game.moved !== 'rigth') {
+            game.move = 'left'
+            if (game.player.points > 0) {
+                game.moved = game.move
+            }
+        } else {
+            if (keyPress === 'ArrowRight' && game.moved !== 'left') {
+                game.move = 'rigth'
+                if (game.player.points > 0) {
+                    game.moved = game.move
+                }
+            } else {
+                if (keyPress === 'ArrowUp' && game.moved !== 'botton') {
+                    game.move = 'top'
+                    if (game.player.points > 0) {
+                        game.moved = game.move
+                    }
+                } else {
+                    if (keyPress === ' ' && game.status == false) {
+                        main()
+                    }
+                }
+            }
         }
-    }
-    if (keyPress === 'ArrowRight' && game.moved !== 'left') {
-        game.move = 'rigth'
-        if(game.player.points > 0){
-            game.moved = game.move
-        }
-    }
-    if (keyPress === 'ArrowUp' && game.moved !== 'botton') {
-        game.move = 'top'
-        if(game.player.points > 0){
-            game.moved = game.move
-        }
-    }
-    if (keyPress === ' ' && game.status == false) {
-        main()
     }
 }
 function movePlayer() {
@@ -91,49 +96,58 @@ function movePlayer() {
             game.fruit = null
             game.player.points++
             document.querySelector(".points").innerHTML = 'Pontos:' + game.player.points
-            game.player.tail.push({x: game.player.x, y: game.player.y})
+            game.player.tail.push({ x: game.player.x, y: game.player.y })
+            if (game.player.points <= 25) {
+                game.time = 300 - (game.player.points * 10)
+                clearInterval(game.loop)
+                game.loop = game.loop = setInterval(movePlayer, game.time)
+                console.log(game.time)
+            }
         }
     }
 
-    if(game.player.tail.length > 0){
-        game.player.tail[game.player.tail.length] = {x: game.player.x, y: game.player.y}
+    if (game.player.tail.length > 0) {
+        game.player.tail[game.player.tail.length] = { x: game.player.x, y: game.player.y }
     }
 
-    while(game.player.tail.length > game.player.points){
+    while (game.player.tail.length > game.player.points) {
         game.player.tail.shift()
-    }   
+    }
 
     if (game.move === 'botton' && game.moved !== 'top') {
-        if(game.player.y + 1 < gameCanvas.height){
+        if (game.player.y + 1 < gameCanvas.height) {
             game.player.y = game.player.y + 1
-        }else{
+        } else {
             game.player.y = 0
         }
-    }
-    if (game.move === 'left' && game.moved !== 'rigth') {
-        if(game.player.x - 1 >= 0){
-            game.player.x = game.player.x - 1
-        }else{
-            game.player.x = gameCanvas.width
-        }
-    }
-    if (game.move === 'rigth' && game.moved !== 'left') {
-        if( game.player.x + 1 < gameCanvas.width){
-            game.player.x = game.player.x + 1
-        }else{
-            game.player.x = 0
-        }
-    }
-    if (game.move === 'top' && game.moved !== 'botton') {
-        if(game.player.y - 1 >= 0){
-            game.player.y = game.player.y - 1
-        }else{
-            game.player.y = gameCanvas.height
+    } else {
+        if (game.move === 'left' && game.moved !== 'rigth') {
+            if (game.player.x - 1 >= 0) {
+                game.player.x = game.player.x - 1
+            } else {
+                game.player.x = gameCanvas.width
+            }
+        } else {
+            if (game.move === 'rigth' && game.moved !== 'left') {
+                if (game.player.x + 1 < gameCanvas.width) {
+                    game.player.x = game.player.x + 1
+                } else {
+                    game.player.x = 0
+                }
+            } else {
+                if (game.move === 'top' && game.moved !== 'botton') {
+                    if (game.player.y - 1 >= 0) {
+                        game.player.y = game.player.y - 1
+                    } else {
+                        game.player.y = gameCanvas.height
+                    }
+                }
+            }
         }
     }
 
-    for(let i = 0; i < game.player.tail.length; i++){
-        if(game.player.tail[i].x == game.player.x && game.player.y == game.player.tail[i].y){
+    for (let i = 0; i < game.player.tail.length; i++) {
+        if (game.player.tail[i].x == game.player.x && game.player.y == game.player.tail[i].y) {
             clearInterval(game.loop)
             document.querySelector(".points").innerHTML = 'Game Over: ' + game.player.points + ' pontos'
             document.getElementById('play').classList.remove('some')
