4b7ce9397a7c13d2d237e7fae63ab77fb5617839
Luis Ricardo <luis.ricardo@tech4h.com.br>
2022-02-03 21:31:46 -0300

    secure

--- pages/api/send.ts
+++ pages/api/send.ts
@@ -3,7 +3,9 @@ import { NextApiRequest, NextApiResponse } from "next";
 const isDev = !process.env.AWS_REGION;
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASSWORD,
